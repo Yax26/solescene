d8c6d0e6c1883107a46fcf39087e270384c80d53
Yax26 <yaxbarot2605@gmail.com>
2024-10-27 23:27:43 -0400

    structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and git
-        </a>
-      </header>
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
 
+function Header() {
+  return (
+    <header>
+      <div class="logo">LOGO</div>
+      <nav>
+        <a href="#">Home</a>
+        <input type="search" placeholder="Search..." />
+      </nav>
+      <div class="user-cart">
+        <div class="cart">Cart</div>
+        <div class="user">User</div>
+      </div>
+    </header>
+  );
+}
+
+function Body() {
+  return (
+    <main>
+      <div class="filters">Filters</div>
+      <section class="products">
+        <article class="product">
+          <div class="product-image">Image</div>
+          <div class="product-details">
+            <p class="price">Price</p>
+            <button>Add to Cart</button>
+          </div>
+        </article>
+        <article class="product">
+          <div class="product-image">Image</div>
+          <div class="product-details">
+            <p class="price">Price</p>
+            <button>Add to Cart</button>
+          </div>
+        </article>
+      </section>
+      <aside class="coupons">Coupons</aside>
+    </main>
+  );
+}
+
+function Footer() {
+  return <footer>Footer content here.</footer>;
+}
 export default App;
